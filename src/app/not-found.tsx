53e46e3b7786103f7fbe234e0743c2a574ab1c0c
Sebastian Calo <smilingcalo@gmail.com>
2024-11-11 13:12:14 -0500

    Escaped apostrophe

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import TextLink from "./util/link";
 export default function NotFound() {
 	return (
 		<div className="errorWrapper">
-			<p>Oops! Looks like you took a wrong turn. This page doesn't exist!</p>
+			<p>Oops! Looks like you took a wrong turn. This page doesn&apos;t exist!</p>
 			<TextLink href="./">Return home</TextLink>
 		</div>
 	);
